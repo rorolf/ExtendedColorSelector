6fb2c0641c8dbcca22dc3a001e313993a492bdcc
Me <None>
2026-04-11 13:03:02 +0200

    In MidiListener, added MidiThreadReceiver for explicit polling of Midi events through PortMidi, added explicit Thread handling and connections for MidiEvent creation and propagation, removed RtMidi functionality

--- src/EXMIDIListener.cpp
+++ src/EXMIDIListener.cpp
@@ -3,50 +3,117 @@
 #include <QMetaObject>
 #include <QDebug>
 
+#include <QLibrary>
+#include <qthread.h>
+#include <QDateTime>
+
+//#include <RtMidi.h>
+#include <vector>
+
 MidiListener::MidiListener(QObject* parent)
-    : QObject(parent), midiIn(new RtMidiIn())
+    : QObject(parent)//, midiIn(new RtMidiIn())
+    , midiThread(nullptr)
 {
+
+    //################################################################################
+    //##  PortMidi Test
+    //################################################################################
+
+    QString deviceName = "LPD8 mk2 MIDI 1";
+    this->startOrReplaceMidiReceiver(deviceName);
+
+
+    //################################################################################
+    //## RtMidi Test
+    //################################################################################
+
+    // QLibrary myLib("mylib");
+    // typedef void (*MyPrototype)();
+    // MyPrototype myFunction = (MyPrototype) myLib.resolve("mysymbol");
+    // if (myFunction)
+    //     myFunction();
+
+    // QLibrary rtMidiLib("librtmidi.so");
+    // typedef RtMidiIn* (*MidiF)();
+    // MidiF getRtMidiIn = (MidiF)rtMidiLib.resolve("rtmidi_in_create");
+    //
+    // if (getRtMidiIn) {
+    //     //midiIn = new RtMidiIn();
+    //     midiIn = getRtMidiIn();
+    // } else {
+    //     qDebug() << "Failed to load RtMidi dynamically";
+    //     throw std::runtime_error("Failed to load RtMidi dynamically");
+    // }
+
+    // midiIn = new RtMidiIn();
+    //
+    // std::vector<RtMidi::Api> apis;
+    // RtMidi::getCompiledApi(apis);
+    // qDebug() << "Available APIs:";
+    // for (auto api : apis) {
+    //     qDebug() << QString::fromStdString(RtMidi::getApiName(api));
+    // }
+    // qDebug() << "    Current API:" << QString::fromStdString(RtMidi::getApiName(midiIn->getCurrentApi()));
+
+
     // Do not ignore sysex messages
     // Do not ignore timing messages
-    // DO ignore active sensing messages (continuous stream of current state)
-    midiIn->ignoreTypes(false, false, true);
+    // Do not ignore active sensing messages (continuous stream of current state)
+    // midiIn->ignoreTypes(false, false, false);
 }
 
 MidiListener::~MidiListener()
 {
     closePort();
-    delete midiIn;
+    // delete midiIn;
+    midiThread->quit();
 }
 
 QStringList MidiListener::availableInputPorts() const
 {
+    // QStringList ports;
+    // unsigned int nPorts = midiIn->getPortCount();
+    // for (unsigned int i = 0; i < nPorts; ++i) {
+    //     qDebug() << QString::fromStdString(midiIn->getPortName(i));
+    //     ports << QString::fromStdString(midiIn->getPortName(i));
+    // }
+    // qDebug() << "All Ports collected";
+
     QStringList ports;
-    unsigned int nPorts = midiIn->getPortCount();
-    for (unsigned int i = 0; i < nPorts; ++i) {
-        ports << QString::fromStdString(midiIn->getPortName(i));
+    for (int k = 0; k < Pm_CountDevices(); ++k) {
+        const PmDeviceInfo* devInfo = Pm_GetDeviceInfo(k);
+
+        if (!devInfo->output) {
+            qDebug() << "PortMidi available input device: " << QString::number(k) << " '" << devInfo->interf << "', '" << devInfo->name << "'";
+            ports << QString::fromStdString(devInfo->name);
+        } else {
+            qDebug() << "PortMidi available output device: '" << devInfo->interf << "', '" << devInfo->name << "'";
+        }
     }
+
     return ports;
 }
 
 void MidiListener::openPort(int index)
 {
-    closePort();  // Close any existing port
-    try {
-        midiIn->openPort(index);
-        midiIn->setCallback(&MidiListener::midiCallback, this);
-    } catch (RtMidiError& e) {
-        emit sigErrorOccurred(QString::fromStdString(e.getMessage()));
-    }
+    Q_UNUSED(index);
+    // closePort();  // Close any existing port
+    // try {
+    //     midiIn->openPort(index);
+    //     midiIn->setCallback(&MidiListener::midiCallback, this);
+    // } catch (RtMidiError& e) {
+    //     emit sigErrorOccurred(QString::fromStdString(e.getMessage()));
+    // }
 }
 
 void MidiListener::closePort()
 {
-    if (midiIn->isPortOpen()) {
-        midiIn->closePort();
-    }
+    // if (midiIn->isPortOpen()) {
+    //     midiIn->closePort();
+    // }
 }
 
-void MidiListener::midiCallback(double, std::vector<unsigned char>* message, void* userData)
+void MidiListener::rtMidiCallback(double, std::vector<unsigned char>* message, void* userData)
 {
     auto* self = static_cast<MidiListener*>(userData);
     if (!self) return;
@@ -63,6 +130,90 @@ void MidiListener::midiCallback(double, std::vector<unsigned char>* message, voi
     }
 }
 
+void MidiListener::startOrReplaceMidiReceiver(const QString& deviceName) {
+    PmDeviceID pmidiInputID = Pm_GetDefaultInputDeviceID();
+    bool deviceFound = false;
+
+    if (Pm_CountDevices() == 0 || pmidiInputID == pmNoDevice) {
+        qDebug() << "Portmidi was unable to find any devices";
+    }
+
+    for (int k = 0; k < Pm_CountDevices(); ++k) {
+        const PmDeviceInfo* devInfo = Pm_GetDeviceInfo(k);
+
+        if (!devInfo->output) {
+            qDebug() << "PortMidi input device:" << QString::number(k) << "'" << devInfo->interf << "', '" << devInfo->name <<"'";
+            if (deviceName == devInfo->name) {
+                qDebug() << "PortMidi successfully found the requested Device";
+                deviceFound = true;
+                pmidiInputID = static_cast<PmDeviceID>(k);
+            }
+        } else if (deviceName == devInfo->name) {
+            qDebug() << "PortMidi found requested input device, but it is an output device.";
+        } else {
+            qDebug() << "PortMidi output device:" << QString::number(k) << "'" << devInfo->interf << "', '" << devInfo->name<<"'";
+        }
+    }
+
+    if (deviceFound && (this->midiThread != nullptr)) {
+        qDebug() << "MidiListener quits its thread.";
+        this->midiThread->quit();
+        this->midiThread = nullptr;
+    }
+    if (!(this->midiThread)) {
+        qDebug() << "MidiListener starts a new thread.";
+        this->midiThread = new QThread(this);
+        qDebug() << "MidiListener creates a new MidiThreadReceiver.";
+        MidiThreadReceiver* midiReceiver = new MidiThreadReceiver(nullptr, pmidiInputID);
+         // on same thread
+        connect(midiThread, &QThread::started, midiReceiver, [midiReceiver](){ midiReceiver->start(); });
+        // between threads
+        connect(midiReceiver, &MidiThreadReceiver::sigMidiMessageArrived, this, &MidiListener::sigMidiMessageArrived, Qt::QueuedConnection);
+        connect(midiReceiver, &MidiThreadReceiver::sigErrorOccurred, this, &MidiListener::sigErrorOccurred, Qt::QueuedConnection);
+        qDebug() << "MidiListener moves the new MidiThreadReceiver to a new thread.";
+        midiReceiver->moveToThread(midiThread);
+        midiThread->start(QThread::TimeCriticalPriority);
+    }
+}
+
+MidiThreadReceiver::~MidiThreadReceiver()
+{
+    qDebug() << "MidiThreadReceiver gets destroyed.";
+    Pm_Close(midiInStream);
+}
+
+void MidiThreadReceiver::start() {
+
+    qDebug() << "MidiThreadReceiver starts operating.";
+    void* DRIVER_INFO = nullptr;
+    int32_t INPUT_BUFFER_SIZE = 1024;
+    PmTimeProcPtr TIME_PROC = nullptr; // originally: ((PmTimeProcPtr) Pt_Time);
+    // #define DEVICE_INFO NULL
+    // #define TIME_PROC ((PmTimeProcPtr) Pt_Time)
+    void* TIME_INFO = nullptr;
+    qDebug() << "MidiThreadReceiver opens stream.";
+    Pm_OpenInput(&midiInStream, this->inputDeviceId, DRIVER_INFO, INPUT_BUFFER_SIZE,
+                     TIME_PROC, TIME_INFO);
+
+    QThread::msleep(500);
+
+    PmEvent midiEventBuffer[1];
+    int msgLength;
+    while (true) {
+        // qDebug() << "MidiThreadReceiver polls stream.";
+        PmError pollStatus = Pm_Poll(midiInStream);
+
+        if (pollStatus == true) {
+            msgLength = Pm_Read(midiInStream, midiEventBuffer, 1);
+            if (msgLength > 0) {
+                MidiEvent midiData = MidiEvent::fromPortMidiMessage(midiEventBuffer[0]);
+                qDebug() << "PortMidi received message:" << midiData.toString();
+                emit sigMidiMessageArrived(midiData);
+            }
+        }
+        QThread::msleep(5);
+    }
+}
 
 
 
--- src/EXMIDIListener.h
+++ src/EXMIDIListener.h
@@ -6,7 +6,38 @@
 #include <kis_shared_ptr.h>
 
 #include "EXMIDIEvent.h"
-#include "RtMidi.h"
+// #include <RtMidi.h>
+// #include <libremidi/libremidi.hpp>
+// #include "portmidi/portmidi.hpp"
+#include <qobject.h>
+// #include "porttime.h" // probably from a separate project
+#include <portmidi.h>
+// #include "pmutil.h"
+
+
+class MidiThreadReceiver : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit MidiThreadReceiver(QObject* parent = nullptr, PmDeviceID openDevice=0)
+    : QObject(parent)
+    , inputDeviceId(openDevice)
+    {};
+    ~MidiThreadReceiver();
+
+public Q_SLOTS:
+    void start();
+
+
+Q_SIGNALS:
+    void sigMidiMessageArrived(const MidiEvent midiData);
+    void sigErrorOccurred(const QString& message);
+
+private:
+    PmStream* midiInStream;
+    PmDeviceID inputDeviceId;
+};
 
 class MidiListener : public QObject
 {
@@ -16,17 +47,25 @@ public:
     explicit MidiListener(QObject* parent = nullptr);
     ~MidiListener();
 
+    QThread* midiThread;
+
     QStringList availableInputPorts() const;
     void openPort(int index);
     void closePort();
+
+    void startOrReplaceMidiReceiver(const QString& deviceName);
 
 Q_SIGNALS:
     void sigMidiMessageArrived(const MidiEvent midiData);
     void sigErrorOccurred(const QString& message);
 
 private:
-    RtMidiIn* midiIn;
-    static void midiCallback(double, std::vector<unsigned char>*, void* userData);
+    // RtMidiIn* midiIn;
+    // libremidi::midi_out
+    static void rtMidiCallback(double, std::vector<unsigned char>*, void* userData);
 };
 
 //typedef KisSharedPtr<MidiListener> MidiListenerSP;
+
+
+
